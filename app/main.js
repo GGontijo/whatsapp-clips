b58613d75408ca027dcdfb8fbef2e570a21f7e80
Dercilio <38219914+GGontijo@users.noreply.github.com>
2024-06-07 07:46:35 -0400

    trying fix with a different webversioncache

--- app/main.js
+++ app/main.js
@@ -42,7 +42,7 @@ const client = new Client({
   webVersionCache: {
     type: "remote",
     remotePath:
-      "https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html",
+      "https://raw.githubusercontent.com/guigo613/alternative-wa-version/main/html/2.2410.1.html",
   },
 });
 
